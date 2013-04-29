7aaa1d5285763fad8d2b23ca55c143dea3170095
Ivan A-R <i@lolhost.org>
2013-04-29 04:36:05 +0400

    sample code

---
+++ firmware/boards/gfxface/board.mk
@@ -0,0 +1,10 @@
+# Required include directories
+BOARDINC = ../../boards/gfxface
+
+# List of all the board related files.
+BOARDSRC = ${BOARDINC}/board.c
+
+
+#openocd:
+#	openocd -f interface/stlink-v1.cfg -f target/stm32f1x_stlink.cfg
+
